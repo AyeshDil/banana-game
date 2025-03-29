c34aab0641a919313c2fa2969cb675f9c5346880
AyeshDil <dilshanwma@gmail.com>
2025-03-29 15:10:43 +0530

    feat: Added max score in dashboard;

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Score;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     
-    public function index() {
-        return view('dashboard.index');
+    public function index() 
+    {
+        // Max score    
+        $maxScore = Score::where('user_id', auth()->user()->id)->max('score');
+
+        return view('dashboard.index', compact('maxScore'));
     }
 
 }
